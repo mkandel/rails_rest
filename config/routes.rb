dbe96ef3aa683a23027bf7ae8e305e8822abdc78
mkandel <marc.kandel.junk@gmail.com>
2019-02-27 20:34:03 -0500

    add root controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'root/index'
+
+  root 'root/index'
+  # http://guides.rubyonrails.org/routing.html
 end
